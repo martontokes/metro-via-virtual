90b8e324266ce9216ecfa57a6a2a0341a44a198a
Marton Tokes <martontokes1@gmail.com>
2023-11-19 15:03:13 +0100

    employed automtic built-in font self-hosting with zero layout shift

--- src/pages/Components/WelcomeScreen.js
+++ src/pages/Components/WelcomeScreen.js
@@ -22,15 +22,15 @@ export default function WelcomeScreen({site}) {
 
         <div id="wcsInnerContainer">
 
-          <h1 id="wcsExhibitionEnglishHeader" className="regular">metro via virtual</h1>
-          <h2 id="wcsExhibitionEnglishSubheader" className="regular">a virtual exhibition from Hong Kong</h2>
+          <h1 id="wcsExhibitionEnglishHeader" className="montserrat">metro via virtual</h1>
+          <h2 id="wcsExhibitionEnglishSubheader" className="montserrat">a virtual exhibition from Hong Kong</h2>
 
           <button id="wcsEnterButtonEnglish" onClick={() => { enterSite('english')} } className="montserrat" ><span id="buttonTextEng">enter exhibition</span></button>
           
-          <h1 id="wcsExhibitionChineseHeader" className="noto notoregular">虛擬都會</h1>
-          <h2 id="wcsExhibitionChineseSubheader" className="noto notoregular">來自香港的線上展覽</h2>
+          <h1 id="wcsExhibitionChineseHeader" className="notoSansHk notoregular">虛擬都會</h1>
+          <h2 id="wcsExhibitionChineseSubheader" className="notoSansHk notoregular">來自香港的線上展覽</h2>
 
-          <button onClick={() => { enterSite('chinese')} } className="noto" id="wcsEnterButtonChinese"><span id="buttonTextZh">進入展覽</span></button>
+          <button onClick={() => { enterSite('chinese')} } className="notoSansHk" id="wcsEnterButtonChinese"><span id="buttonTextZh">進入展覽</span></button>
         
         </div>
         
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,27 @@
-import styles from '@/../../styles/global.css'
-import contentStyles from '@/../../styles/welcomeScreen.css'
+import styles from 'styles/global.css'
+import contentStyles from 'styles/welcomeScreen.css'
+import { Montserrat, Noto_Sans_HK } from 'next/font/google'
+
+const montserrat = Montserrat({ subsets: ['latin']});
+const notoSansHk = Noto_Sans_HK({ subsets: ['latin']});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+ 
+  return (
+
+    <>
+    <style jsx global>{`
+        .montserrat {
+          font-family: ${montserrat.style.fontFamily};
+        }
+
+        .notoSansHk {
+          font-family: ${notoSansHk.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+
+  )
 }
 

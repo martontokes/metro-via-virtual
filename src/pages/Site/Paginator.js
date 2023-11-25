29fa0bffdb5d14949af8c3c1405423365ff3794c
Marton Tokes <martontokes1@gmail.com>
2023-11-25 14:39:58 +0100

    paginator added

--- src/pages/Site/Paginator.js
+++ src/pages/Site/Paginator.js
@@ -1,17 +1,17 @@
+import React from "react";
 import { useLayoutEffect } from "react";
 
-export default function Paginator({site}) {
-
+export default function Paginator({ site }) {
   const mapContentToPaginatorIdEng = {
     Statement: 1,
     Essay: 2,
     Curator: 3,
-    Autosave: 4,  
+    Autosave: 4,
     Confidential: 5,
     Illumination: 6,
     Butterflies: 7,
     Domestik: 8,
-  }
+  };
 
   const mapContentToPaginatorIdZh = {
     StatementZh: 1,
@@ -20,33 +20,30 @@ export default function Paginator({site}) {
     IlluminationZh: 6,
     ButterfliesZh: 7,
     DomestikZh: 8,
-  }
+  };
 
   const paginatorElements = () => {
-
-    if (site.language == 'english') {
-
-      return 
-      mapContentToPaginatorIdEng.values.map((number, index) => 
-      <ellipse rx="50px" ry="50px"></ellipse>
-      )
+    if (site.language === "english") {
+      return Object.entries(mapContentToPaginatorIdEng).map(([key, value], index) => (
+        <div className="paginatorCircle"></div>
+      ));
     } else {
-      
+      // Handle other cases or return null
+      return null;
     }
-  }
+  };
 
   useLayoutEffect(() => {
-
-  }, [])
+    // You might want to include some logic here
+  }, []);
 
   return (
-
     <>
-
       <div id="paginator">
-      {paginatorElements}
+      <div id="paginatorCircleContainer">
+        {paginatorElements()}
       </div>
-
+      </div>
     </>
   );
 }

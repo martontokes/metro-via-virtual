994226a1d40a46e257bf695edc740f39dec2b46d
Marton Tokes <martontokes1@gmail.com>
2023-11-25 17:36:48 +0100

    menu icon replaced

--- src/pages/Site/HamburgerMenu.js
+++ src/pages/Site/HamburgerMenu.js
@@ -1,11 +1,19 @@
-export default function Header({site}) {
+export default function HamburgerMenu({site}) {
 
   function toggleMenu() {
+
+    document.getElementById("hamburgerMenu").classList.add("opacity0");
+
+    // setTimeout(() => {    
+      
+      document.getElementById("hamburgerMenu").classList.remove("opacity0");
+      document.getElementById("burgerImg").classList.toggle("burgerOff");
+    // }, 150)
 
     document.getElementById("menu").classList.toggle("menuVisible");
     window.scrollTo(0, 0);
     document.getElementById("content").classList.toggle("invisible");
-    document.getElementById("burgerImg").classList.toggle("burgerOff");
+    document.getElementById("paginator").classList.toggle("invisible");
 
   }
 
@@ -13,7 +21,7 @@ export default function Header({site}) {
 
     <>
 
-      <button onClick={toggleMenu} id="hamburgerMenu"><img id="burgerImg" src="/burgermenu.svg"></img></button>
+      <button onClick={toggleMenu} id="hamburgerMenu"><img id="burgerImg" src="/hamburgerMenu.svg"></img></button>
 
     </>
   );
--- src/pages/Site/Menu/menuItemClick.mjs
+++ src/pages/Site/Menu/menuItemClick.mjs
@@ -3,5 +3,6 @@ export default function menuItemClick(site, pageName) {
   site.setActivePage(pageName);
   document.getElementById("menu").classList.remove("menuVisible");
   document.getElementById("content").classList.remove("invisible");
-
+  document.getElementById("paginator").classList.remove("invisible");
+  document.getElementById("burgerImg").classList.toggle("burgerOff");
 }

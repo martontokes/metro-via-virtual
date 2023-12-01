52c48dab0513dd29d480f5ebca3cc6300375ae70
Marton Tokes <martontokes1@gmail.com>
2023-12-01 13:52:57 +0100

    buddha images overlay perfect

--- src/pages/Site/Content/English/Artworks/Confidential.js
+++ src/pages/Site/Content/English/Artworks/Confidential.js
@@ -8,11 +8,22 @@ export default function Confidential() {
    
 <div id="description">
         <h3>Confidential Records: <br />Dual Metropolitans (2016 - 2018)</h3>
-        <iframe id="confidentialContent" title="vimeo-player" frameBorder="no" src="https://player.vimeo.com/video/297653544?h=4ccbbb86fe" fullscreen allowFullScreen mozallowfullscreen webkitallowfullscreen allow="autoplay; fullscreen" ></iframe>
         <div className="artistsBox">
           <h4>Vvzela Kook</h4>
           <a href="https://www.vvzela.co" target="_blank">www.vvzela.co</a>
           </div>
+        <>
+  <iframe
+    src="https://player.vimeo.com/video/297653544?h=4ccbbb86fe"
+    width={"100%"}
+    height={180}
+    frameBorder={0}
+    allow="autoplay; fullscreen; picture-in-picture"
+    allowFullScreen=""
+  />
+</>
+
+       
           <p style={{paddingBottom: "60px"}}>Confidential Records: Dual Metropolitans utilizes 3D animation to reconstruct the cyberspace of Kowloon Walled City and imagine what it would be like in the future governed by artificial intelligence. In reality, as an enclave inside Hong Kong during British colonization, Kowloon Walled City developed into a huge and comprehensive urban system. Ungoverned and outlandish, it has long served as an inspiration for cyberpunk-themed artworks. Kook’s project combines history, myth, and sci-fi narratives with digital media to dig into the aesthetics and architecture of dystopia.
           Vvzela Kook is a new media artist who mainly works in audiovisual mediums, including performance, theater, generative art and drawing. Kook’s works combine technology with her artistic practice to reproduce and convert urban cityscapes into an integrated virtual experience, guiding the audience on a cybernetic journey. Kook has participated and shown her works in Kathmandu Triennial, MoCA Taipei, Microwave International Media Festival (Hong Kong), PuSh Performing Art Festival (Vancouver), and Mill6 Foundation (Hong Kong)</p>
           </div>
--- src/pages/Site/Content/English/Artworks/Illumination.js
+++ src/pages/Site/Content/English/Artworks/Illumination.js
@@ -5,7 +5,7 @@ export default function Illumination() {
     useEffect(() => {
         
         // Start slideshow on mounting
-        const intervalId = setInterval(illuminationSlideshow, 4200);
+        const intervalId = setInterval(illuminationSlideshow, 1000);
         return () => clearInterval(intervalId);
 
     }, []);
@@ -31,10 +31,16 @@ function IlluminationDescription() {
           <a href="https://www.katsuryna.com" target="_blank">www.katsuryna.com</a>
           </div>
           <p>Illumination presents the transformation of Buddha’s face in five steps. Through the nuanced changes of light and colors, the Buddha’s varied looks reflect the different spiritual spaces that Suryna has experienced through living in Hong Kong.</p>
-          
-          <IlluminationContent />
+<br />    <div id="buddhacontainer">
+            <img className="buddha" id="buddha1" src="/1.png" ></img>
+            <img className="buddha" id="buddha2" src="/2.png" ></img>
+            <img className="buddha" id="buddha3" src="/3.png" ></img>
+            <img className="buddha" id="buddha4" src="/4.png" ></img>
+            <img className="buddha" id="buddha5" src="/5.png" ></img>
+            </div>
           <p> Lit by warm natural light, the benign face of Buddha is the image Hong Kong aims to achieve via the many Buddha statues in the city. In contrast, inspired by the faces of people on the streets as they stare in concentration at their phones, the demonic look of Buddha lit by cold screen light mirrors an unflattering persona we often encounter/perform online through anonymous identity. The animation is made in collaboration with Sandu Cojocari.
-    Kat Suryna is an international figurative artist with a background in academic philosophy. She follows the tradition of Russian academic drawing and painting, in which she was formally instructed. Her work has been exhibited in Estonia, Hungary, Austria, and Hong Kong. Her art focuses on the human body, organic patterns in nature, and architectural narratives.</p>
+    <br /><br />Kat Suryna is an international figurative artist with a background in academic philosophy. She follows the tradition of Russian academic drawing and painting, in which she was formally instructed. Her work has been exhibited in Estonia, Hungary, Austria, and Hong Kong. Her art focuses on the human body, organic patterns in nature, and architectural narratives.</p>
+
         </div>
 
           </>
@@ -42,43 +48,6 @@ function IlluminationDescription() {
 
 }
 
-function IlluminationContent() {
-
-    if (imagesLoaded === undefined) {
-        var imagesLoaded = 0;
-        }
-      
-        function displayImagesOnLoad() {
-            imagesLoaded += 1;
-            if (imagesLoaded == 5) {
-                setTimeout(() => {
-                    document.getElementById("autosaveLoader").style.opacity = 0;
-                    setTimeout(() => {
-                        document.getElementById("buddhacontainer").style.display = 'block';
-                        document.getElementById("buddhacontainer").style.opacity = 1;
-                    }, 1000);
-                    
-                }, 2000);
-
-            }
-        }
-    
-    return (
-
-        <>
-            <div id="loaderContainer">
-            <div id="autosaveLoader"></div>
-            </div>
-            <div id="buddhacontainer">
-            <img className="buddha" id="buddha1" src="/1.png" onLoad={displayImagesOnLoad}></img>
-            <img className="buddha" id="buddha2" src="/2.png" onLoad={displayImagesOnLoad}></img>
-            <img className="buddha" id="buddha3" src="/3.png" onLoad={displayImagesOnLoad}></img>
-            <img className="buddha" id="buddha4" src="/4.png" onLoad={displayImagesOnLoad}></img>
-            <img className="buddha" id="buddha5" src="/5.png" onLoad={displayImagesOnLoad}></img>
-            </div>
-        </>
-    )
-}
 
 const illuminationSlideshow = (function () {
 

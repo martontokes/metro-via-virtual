fbbd010022cd7dee19c52e45392ea025df932d96
Marton Tokes <martontokes1@gmail.com>
2023-12-07 16:38:20 +0100

    butterflies now responsive on mobile

--- src/pages/Site/Content/English/Artworks/Butterflies.js
+++ src/pages/Site/Content/English/Artworks/Butterflies.js
@@ -3,49 +3,19 @@ import { useState, useEffect } from "react"
 export default function Butterflies() {
 
     const [isFullscreen, setFullscreen] = useState(false);
-    const butterfliesFrame = (<iframe id="butterfliesFrame" frameBorder="no" onLoad={displayVideo} src="/butterflies/0104b/index.htm"></iframe>);
 
-    const toFullscreen = () => {
-
-      setFullscreen(true);
-      document.getElementById("butterfliesExperience").style.pointerEvents = "initial";
-      document.getElementById("butterfliesExperience").style.opacity = 1;
-
-    }
-
-    const attachClickHandler = () => {
-      document.getElementById("butterfliesToFullscreenButton").addEventListener('click', () => {
-
-      })
-    }
-
-    function displayVideo() {
-
-      document.getElementById("butterfliesFrame").style.opacity = 1;
-    }
-
-    function closeFullscreen() {
-      document.getElementById("butterfliesExperience").style.opacity = 0;
-      setTimeout(() => { document.getElementById("butterfliesExperience").style.display = "none" }, 2000);
-    }
-  
     return (
       
       <>
           <div id="description">
-
             <h3>Butterflies on the Wheel (2020)</h3>
             <div className="artistsBox">
             <h4>Elaine Wong</h4>
             <a href="https://www.miss-wong.com" target="_blank">www.miss-wong.com</a>
             </div>
+            <iframe src="butterflies/index.htm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
             <p>Butterflies on the Wheel hails from a cruel imagery – the butterflies flew into a blender and broke their wings. The artwork aims to experiment with videography regarding different forms of presentation through interaction with space. Wong videotaped the cityscape of Hong Kong, and shattered the moving images into broken-down frames. She then projected the images onto her own living room wall. The polygonal and limited space generated multiple reflections, resulting in the fragmentalization of images. It seems to critique the fractured conditions of life caused by the deteriorating spatial capitalization in the city, or perhaps contemplates the relationship between women and the imagery of claustrophobia that Wong has also explored.
             Elaine Wong explores and unveils the manifolds of daily encounters and inner conditions. She regards her practice as an investigation of the potentials of art beyond representation, its relation to sensation, documentation and experience. Her works have been shown internationally, 107 Projects (Australia), the Hong Kong Heritage Museum, Oi! Street Art Space (Hong Kong), EXIS Korea, and Poland Szczecin European Film Festival.</p>
-            <button id="butterfliesToFullscreenButton" style={{width: "50px", height: "50px"}} onClick={toFullscreen}>ButterfliesToFullscreen</button>
-            <div id="butterfliesExperience">
-            <button onClick={closeFullscreen}></button>
-            { isFullscreen ? butterfliesFrame : <></>  }         
-          </div>
         </div>
      </>
     )
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+import { useEffect } from 'react'
+
 export default function Document() {
+
+useEffect(() => {
+  const panorama = new PANOLENS.ImagePanorama( '@/public/butterflies/0104b/media/butterflies.mp4' );
+const viewer = new PANOLENS.Viewer();
+viewer.add( panorama );
+}, []);
 
   return (
 
@@ -11,6 +19,12 @@ export default function Document() {
 />
 
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+
+
+
+
+
         <Head />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" ></meta>
         <title>metro via virtual</title>

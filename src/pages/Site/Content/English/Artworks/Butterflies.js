850ea390ecf8f451ece2bf4febf1bfe81242b616
Marton Tokes <martontokes1@gmail.com>
2023-12-15 17:04:18 +0100

    started butterflies player

--- src/pages/Site/Content/English/Artworks/Butterflies.js
+++ src/pages/Site/Content/English/Artworks/Butterflies.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react"
+import Loader from "pages/Site/Loader";
 
 export default function Butterflies({swiper}) {
 
-    const [isFullscreen, setFullscreen] = useState(false);
+    const [butterfliesLaunched, setButterfliesLaunched] = useState(false);
 
     return (
       
@@ -11,18 +12,36 @@ export default function Butterflies({swiper}) {
           <div id="artwork_page">
             <h3>Butterflies on the Wheel (2020)</h3>
             <div className="artistsBox">
-            <h4>Elaine Wong</h4>
-            <a href="https://www.miss-wong.com" target="_blank">www.miss-wong.com</a>
+              <h4>Elaine Wong</h4>
+              <a href="https://www.miss-wong.com" target="_blank">www.miss-wong.com</a>
             </div>
             <p>Butterflies on the Wheel hails from a cruel imagery – the butterflies flew into a blender and broke their wings. The artwork aims to experiment with videography regarding different forms of presentation through interaction with space. Wong videotaped the cityscape of Hong Kong, and shattered the moving images into broken-down frames. She then projected the images onto her own living room wall. The polygonal and limited space generated multiple reflections, resulting in the fragmentalization of images. It seems to critique the fractured conditions of life caused by the deteriorating spatial capitalization in the city, or perhaps contemplates the relationship between women and the imagery of claustrophobia that Wong has also explored.
             Elaine Wong explores and unveils the manifolds of daily encounters and inner conditions. She regards her practice as an investigation of the potentials of art beyond representation, its relation to sensation, documentation and experience. Her works have been shown internationally, 107 Projects (Australia), the Hong Kong Heritage Museum, Oi! Street Art Space (Hong Kong), EXIS Korea, and Poland Szczecin European Film Festival.</p>
-            { (swiper.activeIndex == 4) ? <iframe src="/butterflies/index.html" style={{width: (window.innerWidth - 48) + 'px'}}frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe> : null }
-        </div>
+            <button onClick={() => {launchButterflies(true)}}>Watch Butterflies</button>
+            { butterfliesLaunched ? <ButterfliesPlayer props={setButterfliesLaunched} /> : null }
+          </div>
      </>
+
     )
 
 }
 
-function ButterfliesPlayer() {
-  
+function ButterfliesPlayer({setButterfliesLaunched}) {
+
+  const [hasLoaded, setLoaded] = useState(false);
+
+  function onIframeLoad() {
+    document.getElementById("loaderContainer").remove;
+    document.getElementById("butterfliesiframe"); //make visible//
+    setLoaded(true);
+  }
+ 
+  return (
+    <>
+    <Loader />
+    <iframe id="butterfliesiframe" onLoad={onIframeLoad} src="/butterflies/index.html" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+    <button onClick={setButterfliesLaunched(false)}></button>
+    </>
+  )
+
 }

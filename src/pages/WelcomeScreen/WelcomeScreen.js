408aefbfa6bb245176b8b09d8122e84960e95e1d
Marton Tokes <martontokes1@gmail.com>
2023-12-20 12:26:28 +0100

    welcome screen responsive and functional on all intented sizes

--- src/pages/WelcomeScreen/WelcomeScreen.js
+++ src/pages/WelcomeScreen/WelcomeScreen.js
@@ -9,11 +9,11 @@ export default function WelcomeScreen({site}) {
         <div id="wcsOuterContainer">
           <div id="wcsInnerContainer">
             <div className="innerContentContainer">
-            <h1 id="wcsExhibitionEnglishHeader" className="montserrat">metro <br />via virtual</h1>
+            <h1 id="wcsExhibitionEnglishHeader" className="montserrat">metro<br/>via virtual</h1>
             <h2 id="wcsExhibitionEnglishSubheader" className="montserrat">a virtual exhibition<br /> from Hong Kong</h2>
             <button id="wcsEnterButtonEnglish" onClick={ () => enterSite(site) } className="montserrat" ><span id="buttonTextEng">enter exhibition</span></button>
             </div>
-            <div className="innerContentContainer">
+            <div className="innerContentContainer" id="iCC2">
             <h1 id="wcsExhibitionChineseHeader" className="notoSansHk notoregular">虛擬都會</h1>
             <h2 id="wcsExhibitionChineseSubheader" className="notoSansHk notoregular">來自香港的線上展覽</h2>
             <button onClick={ () => enterSiteZh(site) } className="notoSansHk" id="wcsEnterButtonChinese"><span id="buttonTextZh">進入展覽</span></button>       

fff35e22d485b4dbb1f42ab50b072f2085a88fd0
Marton Tokes <martontokes1@gmail.com>
2023-12-28 16:00:14 +0100

    removed all components to rebuild clean

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react"
-import { isMobile } from "react-device-detect"
 
-import ExhibitionMenu from "./Site/ExhibitionMenu"
+import Menu from "./Site/ExhibitionMenu"
 import Header from "./Site/Header"
 import Exhibition from "./Site/Exhibition"
 
@@ -13,16 +12,17 @@ export default function App() {
 
   const site = { language, setLanguage, exhibition, setExhibition, artworks, setArtworks };
 
-        return (
+    return (
 
-          <>
+      <>
 
-              <Header site={site} />
-              <ExhibitionMenu site={site}  />
-              <Exhibition site={site} />
+        <Header site={site} />
+        <Menu site={site}  />
+        <Exhibition site={site} />
     
-          </>
+      </>
 
-        )
+    )
 
 }
+

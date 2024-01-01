fb747e080c76ab63e788c0c5de537f9311c8993c
Marton Tokes <martontokes1@gmail.com>
2024-01-01 13:07:38 +0100

    bg dark

---
+++ src/components/Scrollbar.js
@@ -0,0 +1,12 @@
+export default function Scrollbar() {
+
+  return (
+
+    <>
+      <svg width="400" height="110">
+        <rect width="300" height="100" style="fill:rgb(0,0,0);stroke-width:3;stroke:rgb(0,0,0)" />
+      </svg>
+    </>
+
+  )
+}

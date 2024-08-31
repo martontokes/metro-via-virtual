ac7ef9dc3c48ca68e0e4b67fa18de078505afe61
Marton Tokes <martontokes1@gmail.com>
2024-08-31 18:19:31 +0200

    restored working version - proceed to styling and scrollbar

--- modules/contentWindowHeightSetters.mjs
+++ modules/contentWindowHeightSetters.mjs
@@ -1,7 +1,33 @@
 export function setContentWindowSize() {
 
+  let contentWindow = document.querySelector(".fluidHeightContentWindow");
+  let contentWindowComputedStyle = window.getComputedStyle(contentWindow);
+  let contentWindowMargins = parseFloat(contentWindowComputedStyle.marginTop) + parseFloat(contentWindowComputedStyle.marginBottom);
+  let upperMenu = document.getElementById("mainMenu");
+  let lowerMenu = document.getElementById("artworksMenu");
+  let elementToFetchFluidSize = document.getElementById("placeHolderToFetch100dvhInPixels");
+  let upperMenuRect = upperMenu.getBoundingClientRect();
+  let lowerMenuRect = lowerMenu.getBoundingClientRect();
+  let upperMenuStyle = window.getComputedStyle(upperMenu);
+  let lowerMenuStyle = window.getComputedStyle(lowerMenu);
+  let upperMenuHeightTotal = upperMenuRect.height + (parseFloat(upperMenuStyle.marginTop) + parseFloat(upperMenuStyle.marginBottom));
+  let lowerMenuHeightTotal = lowerMenuRect.height + (parseFloat(lowerMenuStyle.marginTop) + parseFloat(lowerMenuStyle.marginBottom));
+  let fluidHeight = elementToFetchFluidSize.getBoundingClientRect();
+  document.documentElement.style.setProperty('--fluidHeightContentWindowHeight', `${fluidHeight.height - (upperMenuHeightTotal + lowerMenuHeightTotal + contentWindowMargins)}px`);
+
 }
 
 export function setContentWindowSizeOnMobile() {
+
+  let contentWindow = document.querySelector(".fluidHeightContentWindow");
+  let contentWindowComputedStyle = window.getComputedStyle(contentWindow);
+  let contentWindowMargins = parseFloat(contentWindowComputedStyle.marginTop) + parseFloat(contentWindowComputedStyle.marginBottom);
+  let upperMenu = document.getElementById("mainMenu");
+  let elementToFetchFluidSize = document.getElementById("placeHolderToFetch100dvhInPixels");
+  let upperMenuRect = upperMenu.getBoundingClientRect();
+  let upperMenuStyle = window.getComputedStyle(upperMenu);
+  let upperMenuHeightTotal = upperMenuRect.height + (parseFloat(upperMenuStyle.marginTop) + parseFloat(upperMenuStyle.marginBottom));
+  let fluidHeight = elementToFetchFluidSize.getBoundingClientRect();
+  document.documentElement.style.setProperty('--fluidHeightContentWindowHeight', `${fluidHeight.height - (upperMenuHeightTotal + contentWindowMargins)}px`);
 
 }
